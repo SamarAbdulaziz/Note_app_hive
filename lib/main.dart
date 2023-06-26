9d46ec2f5c22ca170b8c316457efff318cdc61c6
Samar <sam_abdulaziz@yahoo.com>
2023-06-26 16:22:09 +0300

    generate the hive adapter and add initFlutter from hive flutter and open box (create collection)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app_tharwat_samy/constant.dart';
 import 'package:notes_app_tharwat_samy/views/Notes_view.dart';
 import 'package:notes_app_tharwat_samy/views/edit_note_view.dart';
 
-void main() {
+void main() async{
+
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
